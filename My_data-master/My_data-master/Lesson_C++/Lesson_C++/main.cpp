a32e9549137bd8e4dc4e23c5fad2970a0a12bcaf
大畑　陽平 <sunpeace1059@gmail.com>
2015-07-11 17:55:32 +0900

    no message

--- My_data-master/My_data-master/Lesson_C++/Lesson_C++/main.cpp
+++ My_data-master/My_data-master/Lesson_C++/Lesson_C++/main.cpp
@@ -2,8 +2,10 @@
 using namespace std;
 
 int Processing(int);
+void Message(void);
 int main(){
 	 int a;
+	 int &A = a;
 	char *s1 = "Sunpeace";
 	char *s2 = "Largefiled";
 	char s3[10] = "★";
@@ -12,4 +14,31 @@ int main(){
 	Processing(a);
 	a = 2;
 	Processing(a);
+
+	cout << A << endl;
+
+	Message();
+
 }
+
+void Message(void){
+	int num = 5;
+	double num2 = 0.01;
+	char str1[128] = { 0 }; //0で埋める
+	char str2[128] = { 0 };
+	char str3[128] = { 0 };
+	sprintf_s(str1, "%d", num);
+	sprintf_s(str2, "現在の所持金%d円", num);
+	sprintf_s(str3, "薄さ%.2fミリ", num2);
+	printf("%s\n", str1);
+	printf("%s\n", str2);
+	printf("%s\n", str3);
+	//数値を文字に変換
+	char Level[128] = { 0 };
+	int LV = 0;
+	sprintf_s(Level, "%d", num);
+	cout << "あなたのLV.は"<<Level<<"です。" << endl;
+	//文字を数値にする
+	LV = atoi(Level);
+	cout << LV << endl;
+};
